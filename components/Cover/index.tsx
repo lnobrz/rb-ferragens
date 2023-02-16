79e9e85da005e755fedd640bba8f4eccdf97eb1a
Lucas Nobre <lucasnobre37@outlook.com>
2023-02-16 20:55:51 -0300

    create Cover component

---
+++ components/Cover/index.tsx
@@ -0,0 +1,12 @@
+import MobileHeader from "../ui/MobileHeader";
+import { CoverContainer } from "./styles";
+
+const Cover = () => {
+  return (
+    <CoverContainer>
+      <MobileHeader />
+    </CoverContainer>
+  );
+};
+
+export default Cover;
---
+++ components/Cover/styles.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+export const CoverContainer = styled.section`
+  background: url("/images/cover.png");
+`;
