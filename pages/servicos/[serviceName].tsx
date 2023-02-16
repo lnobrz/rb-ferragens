7100ee240d33b1f9a3cb7cde11bde8566d6ec5fe
Lucas Nobre <lucasnobre37@outlook.com>
2023-02-16 04:05:41 -0300

    remove log

--- pages/servicos/[serviceName].tsx
+++ pages/servicos/[serviceName].tsx
@@ -3,7 +3,6 @@ import { useRouter } from "next/router";
 const Service = () => {
   const router = useRouter();
   const { serviceName } = router.query;
-  console.log(serviceName);
 
   return (
     <h1>
