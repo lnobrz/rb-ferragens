8f4cd44770651c132f76874a44a61cfd20607904
Lucas Nobre <lucasnobre37@outlook.com>
2023-02-17 04:01:02 -0300

    update Cover component

--- components/Cover/index.tsx
+++ components/Cover/index.tsx
@@ -1,10 +1,19 @@
 import MobileHeader from "../ui/MobileHeader";
-import { CoverContainer } from "./styles";
+import { coverParagraph } from "@/storage/strings";
+import { CoverContainer, ParagraphContainer } from "./styles";
+import Paragraph from "../ui/Paragraph";
+import BudgetButton from "../ui/BudgetButton";
+import MainTitle from "../ui/MainTitle";
 
 const Cover = () => {
   return (
     <CoverContainer>
       <MobileHeader />
+      <MainTitle />
+      <ParagraphContainer>
+        <Paragraph text={coverParagraph} />
+        <BudgetButton />
+      </ParagraphContainer>
     </CoverContainer>
   );
 };
--- components/Cover/styles.tsx
+++ components/Cover/styles.tsx
@@ -2,4 +2,20 @@ import styled from "styled-components";
 
 export const CoverContainer = styled.section`
   background: url("/images/cover.png");
+  background-size: cover;
+  background-repeat: no-repeat;
+  height: 100% !important;
+  background-attachment: fixed;
+
+  .highlitedText {
+    color: #cdaf18;
+    margin-left: 10px;
+  }
+`;
+
+export const ParagraphContainer = styled.div`
+  max-width: 60%;
+  margin: 0px 75px 60px 75px;
+  display: flex;
+  flex-flow: column nowrap;
 `;
