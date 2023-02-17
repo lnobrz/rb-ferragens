f20c2170dd93583d96ed538cf29024b133260467
Lucas Nobre <lucasnobre37@outlook.com>
2023-02-17 13:56:44 -0300

    add company metrics to string storage

--- storage/strings.ts
+++ storage/strings.ts
@@ -1,2 +1,20 @@
 export const coverParagraph =
   "Aqui na RB, moldamos as ferragens de acordo com o seu projeto, lhe dando total liberdade para a execução deles.";
+
+export const companyMetrics = [
+  {
+    id: 1,
+    data: "Clientes",
+    amount: 1000,
+  },
+  {
+    id: 2,
+    data: "Pedidos",
+    amount: 2000,
+  },
+  {
+    id: 3,
+    data: "Anos",
+    amount: 4,
+  },
+];
