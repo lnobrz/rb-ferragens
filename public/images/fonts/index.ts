2e27beee86541ec1261d958177dbd2680ade9109
Lucas Nobre <lucasnobre37@outlook.com>
2023-02-17 03:57:47 -0300

    import fonts from google fonts

---
+++ public/images/fonts/index.ts
@@ -0,0 +1,11 @@
+import { Forum, Work_Sans } from "@next/font/google";
+
+export const forum = Forum({
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
+export const workSans = Work_Sans({
+  weight: ["300", "400", "700"],
+  subsets: ["latin"],
+});
