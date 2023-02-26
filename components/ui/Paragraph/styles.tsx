3b8a1c31bf3aa427d0d00274aa8560f080fd49c0
Lucas Nobre <lucasnobre37@outlook.com>
2023-02-26 03:28:58 -0300

    update paragraph tag to h2 tag

--- components/ui/Paragraph/styles.tsx
+++ components/ui/Paragraph/styles.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 import { workSans } from "@/public/fonts";
 
-export const ParagraphContainer = styled.p`
+export const ParagraphContainer = styled.h2`
   font-family: ${workSans.style.fontFamily}, Arial, Helvetica, sans-serif;
   font-style: normal;
   font-weight: 300;
