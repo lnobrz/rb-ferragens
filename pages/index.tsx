53f5070105f5a7724ebaf91575f586450fcca3a4
Lucas Nobre <lucasnobre37@outlook.com>
2023-02-27 03:29:29 -0300

    use IndexSection component to generate ProductSession component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,27 @@
 import Head from "next/head";
 import Cover from "@/components/Cover";
-import { metaDescriptions } from "@/storage/strings";
+import { metaDescriptions } from "@/storage/data";
+import { metaTitles } from "@/storage/data";
 import CompanyMetrics from "@/components/CompanyMetrics";
+import IndexSection from "@/components/ui/IndexSection";
+import { paragraphs } from "@/storage/data";
+import { products } from "@/storage/data";
 
 const Home = () => {
   return (
     <>
       <Head>
         <meta name="description" content={metaDescriptions.index} />
-        <title>Loja de Ferragens | Ferro | Aço - RB Ferragens</title>
+        <title>{metaTitles.index}</title>
       </Head>
       <Cover />
       <CompanyMetrics />
+      <IndexSection
+        titleDecorationAlign="right"
+        sectionTitle="produtos"
+        sectionParagraph={paragraphs.indexProductSession}
+        sectionArr={products}
+      />
     </>
   );
 };
