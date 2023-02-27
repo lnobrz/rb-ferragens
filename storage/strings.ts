3668c4843064be857a3bd41a229400bd935cd529
Lucas Nobre <lucasnobre37@outlook.com>
2023-02-27 02:17:39 -0300

    move meta tags content to storage folder

--- storage/strings.ts
+++ storage/strings.ts
@@ -1,3 +1,11 @@
+export const metaDescriptions = {
+  index:
+    "A RB Ferragens, localizada em Fortaleza - CE, é especialista na venda, corte e dobra de ferragens voltadas para a área da construção civil.",
+};
+
+export const metaKeywords =
+  "barra de ferro, ferro e aço, malha pop, loja de ferragens, rb ferragens, vergalhão, corte e dobra de chapas, arames, treliça, ferragens, barra de ferro, chapas de ferro, malha de ferro";
+
 export const coverParagraph =
   "Atendendo Fortaleza - CE e região metropolitana, vendemos desde colunas até vigas de ferro e aço com as medidas ideais para o seu projeto.";
 
