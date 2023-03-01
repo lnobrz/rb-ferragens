5af1641d6a89916939323110f4f6dc28afc7c4c8
Lucas Nobre <lucasnobre37@outlook.com>
2023-03-01 02:06:01 -0300

    create FooterAddresses component

---
+++ components/ui/FooterAddresses/index.tsx
@@ -0,0 +1,17 @@
+import { AddressesContainer } from "./styles";
+import Paragraph from "../Paragraph";
+import { address } from "@/storage/data";
+
+const FooterAddresses = () => {
+  return (
+    <AddressesContainer>
+      <Paragraph content={address.street} className="footerAddress" />
+      <Paragraph
+        content={address.neighborhoodCityState}
+        className="footerAddress"
+      />
+    </AddressesContainer>
+  );
+};
+
+export default FooterAddresses;
---
+++ components/ui/FooterAddresses/styles.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const AddressesContainer = styled.address`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-end;
+
+  .footerAddress {
+    margin: 0;
+    color: #949186;
+  }
+`;
