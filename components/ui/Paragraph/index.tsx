767434030a9b9d326106ca23be99e91353d3dbf3
Lucas Nobre <lucasnobre37@outlook.com>
2023-03-09 02:11:03 -0300

    add Paragraph component animations

--- components/ui/Paragraph/index.tsx
+++ components/ui/Paragraph/index.tsx
@@ -5,9 +5,29 @@ type Paragraph = {
   className?: string;
 };
 
+const paragraphVariants = {
+  hidden: {
+    opacity: 0,
+    x: "-5vh",
+  },
+  show: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      duration: 1,
+      bounce: 0,
+      stiffness: 50,
+    },
+  },
+};
+
 const Paragraph = ({ content, className }: Paragraph) => {
   return (
-    <ParagraphContainer className={className ? className : ""}>
+    <ParagraphContainer
+      variants={paragraphVariants}
+      className={className ? className : ""}
+    >
       {content}
     </ParagraphContainer>
   );
--- components/ui/Paragraph/styles.tsx
+++ components/ui/Paragraph/styles.tsx
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 import { kanit } from "@/public/fonts";
+import { motion } from "framer-motion";
 
-export const ParagraphContainer = styled.p`
+export const ParagraphContainer = styled(motion.p)`
   font-family: ${kanit.style.fontFamily}, Arial, Helvetica, sans-serif;
   font-style: normal;
   font-weight: 400;
