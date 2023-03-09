a2e172ed8603aa8746f6d4db7da457f91cefe953
Lucas Nobre <lucasnobre37@outlook.com>
2023-03-09 02:09:46 -0300

    remove Footer component from index and add it to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from "next/app";
 import { GlobalStyle } from "@/components/ui/globalStyles";
 import { AnimatePresence } from "framer-motion";
+import MobileHeader from "@/components/ui/MobileHeader";
+import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AnimatePresence>
       <GlobalStyle />
       <Component {...pageProps} />
+      <Footer />
     </AnimatePresence>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import Cover from "@/components/Cover";
 import CompanyMetrics from "@/components/CompanyMetrics";
 import { metaDescriptions, metaTitles } from "@/storage/data";
 import IndexSections from "@/components/IndexSections";
-import Footer from "@/components/Footer";
 import Layout from "@/components/ui/Layout";
 
 const Home = () => {
@@ -16,7 +15,6 @@ const Home = () => {
       <Cover />
       <CompanyMetrics />
       <IndexSections />
-      <Footer />
     </Layout>
   );
 };
