9b232064df94d99a73b664661460bbd1afa25e7d
Lucas Nobre <lucasnobre37@outlook.com>
2023-03-09 02:15:57 -0300

    add transparent background to Mobile Header

--- components/ui/MobileHeader/styles.tsx
+++ components/ui/MobileHeader/styles.tsx
@@ -4,7 +4,7 @@ export const HeaderContainer = styled.header`
   display: flex;
   justify-content: flex-end;
   align-items: center;
-  background: inherit;
+  background: transparent;
 `;
 
 export const LogoContainer = styled.div`
