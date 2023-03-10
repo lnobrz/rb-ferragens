ff398991f115dc11a176924ed4d5824d9bed1f41
Lucas Nobre <lucasnobre37@outlook.com>
2023-03-10 03:11:22 -0300

    add responsiveness to Cover

--- components/Cover/styles.tsx
+++ components/Cover/styles.tsx
@@ -8,13 +8,53 @@ export const CoverContainer = styled.section`
   background-attachment: fixed;
 
   .coverButton {
-    margin: 24px 0 68px 0;
+    margin: 10px 0 68px 0;
   }
 `;
 
 export const ParagraphContainer = styled(motion.div)`
-  max-width: 70%;
-  margin: 0px 75px 60px 75px;
+  max-width: 80%;
+  margin: 0 40px 60px 40px;
   display: flex;
   flex-flow: column nowrap;
+
+  @media (min-width: 400px) {
+    max-width: 70%;
+    margin: 0 80px 60px 80px;
+  }
+
+  @media (min-width: 500px) {
+    max-width: 55%;
+    margin: 0 0 60px 80px;
+  }
+
+  @media (min-width: 600px) {
+    max-width: 45%;
+    margin: 0 0 60px 120px;
+  }
+
+  @media (min-width: 800px) {
+    max-width: 40%;
+    margin: 0 0 60px 140px;
+  }
+
+  @media (min-width: 900px) {
+    margin: 0 0 60px 160px;
+  }
+
+  @media (min-width: 900px) {
+    margin: 0 0 60px 160px;
+  }
+
+  @media (min-width: 1200px) {
+    margin: 0 0 60px 180px;
+  }
+
+  @media (min-width: 1350px) {
+    margin: 0 0 60px 200px;
+  }
+
+  @media (min-width: 1500px) {
+    max-width: 25%;
+  }
 `;
