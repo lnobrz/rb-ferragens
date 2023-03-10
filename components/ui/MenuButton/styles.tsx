fb72a92117a65802593d5af252ff1e949a649bad
Lucas Nobre <lucasnobre37@outlook.com>
2023-03-10 03:10:51 -0300

    add responsiveness to MenuButton

--- components/ui/MenuButton/styles.tsx
+++ components/ui/MenuButton/styles.tsx
@@ -11,4 +11,34 @@ export const MenuButtonContainer = styled(motion.button)`
     position: static !important;
     margin: 30px 20px;
   }
+
+  @media (min-width: 600px) {
+    .menuButton {
+      margin: 20px 40px;
+    }
+  }
+
+  @media (min-width: 800px) {
+    .menuButton {
+      margin: 30px 60px;
+    }
+  }
+
+  @media (min-width: 900px) {
+    .menuButton {
+      margin: 40px 80px;
+    }
+  }
+
+  @media (min-width: 1200px) {
+    .menuButton {
+      margin: 50px 100px;
+    }
+  }
+
+  @media (min-width: 1350px) {
+    .menuButton {
+      margin: 60px 120px;
+    }
+  }
 `;
