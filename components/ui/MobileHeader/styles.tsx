c5f524d8e1b924f3db1d91b0aad6d98c82ec42da
Lucas Nobre <lucasnobre37@outlook.com>
2023-03-10 03:10:30 -0300

    add responsiveness to MobileHeader

--- components/ui/MobileHeader/styles.tsx
+++ components/ui/MobileHeader/styles.tsx
@@ -17,4 +17,34 @@ export const LogoContainer = styled.div`
     margin: 20px;
     position: static;
   }
+
+  @media (min-width: 600px) {
+    .logo {
+      margin: 20px 40px;
+    }
+  }
+
+  @media (min-width: 800px) {
+    .logo {
+      margin: 30px 60px;
+    }
+  }
+
+  @media (min-width: 900px) {
+    .logo {
+      margin: 40px 80px;
+    }
+  }
+
+  @media (min-width: 1200px) {
+    .logo {
+      margin: 50px 100px;
+    }
+  }
+
+  @media (min-width: 1350px) {
+    .logo {
+      margin: 60px 120px;
+    }
+  }
 `;
