bee2f2be6d16f1e7162bce785b535a740e52a29a
Lucas Nobre <lucasnobre37@outlook.com>
2023-03-16 00:44:05 -0300

    remove useless import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from "next/app";
 import { GlobalStyle } from "@/components/ui/globalStyles";
 import { AnimatePresence } from "framer-motion";
-import MobileHeader from "@/components/ui/MobileHeader";
 import Footer from "@/components/Footer";
 
 export default function App({ Component, pageProps }: AppProps) {
