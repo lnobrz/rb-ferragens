895aeb6e1d5c906fac130b81cbc8b8f748b1250a
Lucas Nobre <lucasnobre37@outlook.com>
2023-03-16 00:40:48 -0300

    add responsiveness to Paragraph

--- components/ui/Paragraph/styles.tsx
+++ components/ui/Paragraph/styles.tsx
@@ -2,7 +2,29 @@ import styled from "styled-components";
 import { kanit } from "@/public/fonts";
 import { motion } from "framer-motion";
 
-export const ParagraphContainer = styled(motion.p)`
+export const MotionParagraphContainer = styled(motion.p)`
+  font-family: ${kanit.style.fontFamily}, Arial, Helvetica, sans-serif;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 1em;
+  line-height: 20px;
+  color: #e5dfc3;
+
+  @media (min-width: 500px) {
+    font-size: 1.1em;
+    line-height: 25px;
+  }
+
+  @media (min-width: 700px) {
+    font-size: 1.3em;
+  }
+
+  @media (min-width: 1800px) {
+    font-size: 1.5em;
+  }
+`;
+
+export const ParagraphContainer = styled.p`
   font-family: ${kanit.style.fontFamily}, Arial, Helvetica, sans-serif;
   font-style: normal;
   font-weight: 400;
