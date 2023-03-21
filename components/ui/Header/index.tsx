1dba37813b18e3671bd832b5328137117cb4cd29
Lucas Nobre <lucasnobre37@outlook.com>
2023-03-21 00:54:09 -0300

    add responsiveness to Header

---
+++ components/ui/Header/index.tsx
@@ -0,0 +1,43 @@
+import Logo from "../Logo";
+import MenuButton from "../MenuButton";
+import { HeaderContainer, LogoContainer } from "./styles";
+import { useState } from "react";
+import Menu from "../Menu";
+import { AnimatePresence } from "framer-motion";
+import useDeviceInfo from "@/hooks/useDeviceInfo";
+import MenuBudgetButton from "../MenuBudgetButton";
+
+const MobileHeader = () => {
+  const device = useDeviceInfo();
+  const [showMobileMenu, setShowMobileMenu] = useState(false);
+  const showMenuHandler = () => {
+    setShowMobileMenu((previousValue) => !previousValue);
+  };
+
+  return (
+    <HeaderContainer>
+      <LogoContainer>
+        <Logo logoDirection="left" className="logo" />
+      </LogoContainer>
+      {device === "mobile" && (
+        <>
+          <MenuButton
+            handleClickFunction={showMenuHandler}
+            menuIsOpened={showMobileMenu}
+          />
+          <AnimatePresence>
+            {showMobileMenu && <Menu disableAnimations={false} />}
+          </AnimatePresence>
+        </>
+      )}
+      {device === "desktop" && (
+        <>
+          <Menu disableAnimations={true} />
+          <MenuBudgetButton disableAnimations={true} />
+        </>
+      )}
+    </HeaderContainer>
+  );
+};
+
+export default MobileHeader;
---
+++ components/ui/Header/styles.tsx
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  background: transparent;
+
+  @media (min-width: 1000px) {
+    justify-content: space-between;
+    margin: 0 80px;
+  }
+
+  @media (min-width: 1200px) {
+    margin: 0 100px;
+  }
+
+  @media (min-width: 1350px) {
+    margin: 0 120px;
+  }
+`;
+
+export const LogoContainer = styled.div`
+  width: 50px;
+  height: 29px;
+
+  .logo {
+    max-width: 50px;
+    max-height: 40px;
+    margin: 20px;
+    position: static;
+  }
+
+  @media (min-width: 600px) {
+    .logo {
+      margin: 20px 40px;
+    }
+  }
+
+  @media (min-width: 800px) {
+    .logo {
+      margin: 30px 60px;
+    }
+  }
+
+  @media (min-width: 900px) {
+    .logo {
+      margin: 40px 80px;
+    }
+  }
+
+  @media (min-width: 1200px) {
+    .logo {
+      margin: 50px 100px;
+    }
+  }
+
+  @media (min-width: 1350px) {
+    .logo {
+      margin: 60px 120px;
+    }
+  }
+`;
