8b0eabee619140bc2c61f70bce691421a55700aa
Lucas Nobre <lucasnobre37@outlook.com>
2023-03-27 23:57:24 -0300

    create SectionPage component

---
+++ components/SectionPage/index.tsx
@@ -0,0 +1,84 @@
+import { SectionContainer, CardsContainer } from "./styles";
+import OptionCard from "../ui/OptionCard";
+import SecondaryTitle from "../ui/SecondaryTitle";
+import useDeviceInfo from "@/hooks/useDeviceInfo";
+
+type SectionPageTypes = {
+  pageTitle: string;
+  pageContents: {
+    id: number;
+    name: string;
+    description: string;
+    imageUrl: string;
+  }[];
+};
+
+const mobileSectionPageVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0,
+      mass: 0.1,
+      stiffness: 50,
+      delayChildren: 0.3,
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const desktopSectionPageVariants = {
+  hidden: {
+    opacity: 0,
+  },
+  show: {
+    opacity: 1,
+    transition: {
+      type: "spring",
+      bounce: 0,
+      mass: 0.1,
+      stiffness: 50,
+    },
+  },
+};
+
+const SectionPage = ({ pageTitle, pageContents }: SectionPageTypes) => {
+  const device = useDeviceInfo();
+
+  return (
+    <SectionContainer>
+      <SecondaryTitle
+        decorationAlign="left"
+        content={pageTitle}
+        className="sectionTitle"
+      />
+      <CardsContainer
+        variants={
+          device === "mobile"
+            ? mobileSectionPageVariants
+            : desktopSectionPageVariants
+        }
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+      >
+        {pageContents.map((content) => {
+          return (
+            <OptionCard
+              key={content.id}
+              title={content.name}
+              description={content.description}
+              imageUrl={content.imageUrl}
+            />
+          );
+        })}
+      </CardsContainer>
+    </SectionContainer>
+  );
+};
+
+export default SectionPage;
---
+++ components/SectionPage/styles.tsx
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+import { motion } from "framer-motion";
+
+export const SectionContainer = styled.section`
+  margin-top: 20px;
+
+  @media (min-width: 600px) {
+    margin-top: 30px;
+  }
+
+  @media (min-width: 800px) {
+    margin-top: 40px;
+  }
+
+  .sectionTitle {
+    margin: 0 0 50px 20px !important;
+
+    @media (min-width: 600px) {
+      margin: 0 0 80px 40px !important;
+    }
+
+    @media (min-width: 800px) {
+      margin: 0 0 80px 60px !important;
+    }
+
+    @media (min-width: 900px) {
+      margin: 0 0 80px 80px !important;
+    }
+
+    @media (min-width: 1200px) {
+      margin: 0 0 80px 100px !important;
+    }
+
+    @media (min-width: 1350px) {
+      margin: 0 0 80px 120px !important;
+    }
+  }
+`;
+
+export const CardsContainer = styled(motion.div)`
+  display: flex;
+  flex-flow: column nowrap;
+  justify-content: center;
+  align-items: center;
+  margin: 50px 20px 100px 20px;
+  gap: 30px;
+
+  @media (min-width: 850px) {
+    flex-flow: row wrap;
+    justify-content: flex-start;
+    margin: 50px 60px 100px 60px;
+  }
+
+  @media (min-width: 900px) {
+    margin: 50px 80px 100px 80px;
+  }
+
+  @media (min-width: 1200px) {
+    margin: 50px 100px 100px 100px;
+  }
+
+  @media (min-width: 1350px) {
+    margin: 50px 120px 100px 120px;
+  }
+`;
