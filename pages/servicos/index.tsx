df7e4dbfd8bead8bd779b84b00dd94b47e5f8b51
Lucas Nobre <lucasnobre37@outlook.com>
2023-03-28 00:11:49 -0300

    implement services page

--- pages/servicos/index.tsx
+++ pages/servicos/index.tsx
@@ -1,5 +1,20 @@
+import Head from "next/head";
+import { metaDescriptions, metaTitles } from "@/storage/data";
+import Layout from "@/components/ui/Layout";
+import Header from "@/components/ui/Header";
+import ServicesSection from "@/components/ServicesSection";
+
 const Services = () => {
-  return <h1>services</h1>;
+  return (
+    <Layout>
+      <Head>
+        <meta name="description" content={metaDescriptions.services} />
+        <title>{metaTitles.services}</title>
+      </Head>
+      <Header />
+      <ServicesSection />
+    </Layout>
+  );
 };
 
 export default Services;
