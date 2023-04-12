59255044e7456c26b02f0a7d285ada43f0495458
Lucas Nobre <lucasnobre37@outlook.com>
2023-04-12 01:31:14 -0300

    Update components props

--- components/ProductDetailsSection/index.tsx
+++ components/ProductDetailsSection/index.tsx
@@ -2,7 +2,11 @@ import SecondaryTitle from "../ui/SecondaryTitle";
 import DecoratedImage from "../ui/DecoratedImage";
 import TertiaryTitle from "../ui/TertiaryTitle";
 import Paragraph from "../ui/Paragraph";
-import { SectionContainer, TextGroup, ButtonsContainer } from "./styles";
+import {
+  SectionContainer,
+  InformationContainer,
+  ButtonsContainer,
+} from "./styles";
 import FilledButton from "../ui/FilledButton";
 import ProductTable from "../ui/ProductTable";
 
@@ -14,26 +18,26 @@ type ProductDetailsSection = {
   measurements?: {
     tableHeaders: string[];
     tableData:
-      | {
-          id: number;
-          designation: string;
-          malhaCentimeters: string;
-          bitolaMilimeters: string;
-          pieceWeight: string;
-        }[]
-      | {
-          id: number;
-          bitolaInches: string;
-          bitolaMilimeters: string;
-          barWeight: string;
-          weightPerMeter: string;
-        }[]
-      | {
-          id: number;
-          bitolaMilimeters: string;
-          barWeight: string;
-          weightPerMeter: string;
-        }[];
+    | {
+      id: number;
+      designation: string;
+      malhaCentimeters: string;
+      bitolaMilimeters: string;
+      pieceWeight: string;
+    }[]
+    | {
+      id: number;
+      bitolaInches: string;
+      bitolaMilimeters: string;
+      barWeight: string;
+      weightPerMeter: string;
+    }[]
+    | {
+      id: number;
+      bitolaMilimeters: string;
+      barWeight: string;
+      weightPerMeter: string;
+    }[];
   };
   measurements2?: {
     tableHeaders: string[];
@@ -57,21 +61,17 @@ const ProductDetailsSection = ({
   return (
     <SectionContainer>
       <SecondaryTitle
-        decorationAlign="left"
+        decorationAlign="none"
         content={productName}
         className="productDetailsSecondaryTitle"
       />
       <DecoratedImage className="productDetailsImage" />
-      <TextGroup>
-        <TertiaryTitle decorationAlign="left" content="o que é?" />
+      <InformationContainer>
         <Paragraph content={productDescription} />
-      </TextGroup>
-      <TextGroup>
-        <TertiaryTitle decorationAlign="left" content="onde usar?" />
         <Paragraph content={productRecomendations} />
-      </TextGroup>
+      </InformationContainer>
       {haveCorteEDobra && measurements && (
-        <TextGroup>
+        <InformationContainer>
           <TertiaryTitle decorationAlign="left" content="corte e dobra" />
           <ProductTable
             tableHeaders={measurements.tableHeaders}
@@ -83,18 +83,20 @@ const ProductDetailsSection = ({
               tableData={measurements2.tableData}
             />
           )}
-        </TextGroup>
+        </InformationContainer>
       )}
       <ButtonsContainer>
         <FilledButton
           size="large"
           background="yellow"
           content="Compre Conosco"
+          className="detailsButton"
         />
         <FilledButton
           size="large"
           background="transparent"
           content="Outros Produtos"
+          className="detailsButton"
         />
       </ButtonsContainer>
     </SectionContainer>
