ebc7e6e63c1356c3b75b93e318d2d0c170bf8fa7
Lucas Nobre <lucasnobre37@outlook.com>
2023-04-12 01:31:37 -0300

    adjust ImageContainer responsiveness

--- components/ui/DecoratedImage/styles.tsx
+++ components/ui/DecoratedImage/styles.tsx
@@ -3,8 +3,20 @@ import styled from "styled-components";
 export const ImageContainer = styled.div`
   position: relative;
   width: 100%;
-  height: 400px;
+  height: 300px;
   border-width: 5px 5px 0px 0px;
   border-style: solid;
   border-color: #efdf64;
+
+  @media (min-width: 400px){
+    height: 400px;
+  }
+
+  @media (min-width: 500px){
+    height: 500px;
+  }
+
+  @media (min-width: 1000px){
+    width: 50%;
+  }
 `;
