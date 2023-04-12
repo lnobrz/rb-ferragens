44dff7680c7dce6f4e0651cbdc74738fb750b8c1
Lucas Nobre <lucasnobre37@outlook.com>
2023-04-11 21:41:09 -0300

    add top margin to ProductTable component

--- components/ui/ProductTable/styles.tsx
+++ components/ui/ProductTable/styles.tsx
@@ -1,20 +1,23 @@
 import styled from "styled-components";
 import { kanit } from "@/public/fonts";
 
-
 export const TableContainer = styled.table`
-    width: 100%;
-    color: #e5dfc3;
-    font-family: ${kanit.style.fontFamily};
-    font-size: 16px;
-    text-transform: uppercase;
-    text-align: center;
-    align-items: center;
+  width: 100%;
+  color: #e5dfc3;
+  font-family: ${kanit.style.fontFamily};
+  font-size: 16px;
+  text-transform: uppercase;
+  text-align: center;
+  align-items: center;
+  border: 1px solid #efdf64;
+  margin-top: 50px;
+
+  thead,
+  tbody,
+  tr,
+  th,
+  td {
     border: 1px solid #efdf64;
-
-    thead, tbody, tr, th, td {
-        border: 1px solid #efdf64;
-        padding: 10px;
-    }
-
+    padding: 10px;
+  }
 `;
