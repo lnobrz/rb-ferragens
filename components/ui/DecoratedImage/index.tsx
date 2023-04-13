f46e849b2a6805eb2d7aca8e702a10712e1ac40b
Lucas Nobre <lucasnobre37@outlook.com>
2023-04-12 21:20:54 -0300

    remove DecoratedImage border and add support to add image urls

--- components/ui/DecoratedImage/index.tsx
+++ components/ui/DecoratedImage/index.tsx
@@ -2,15 +2,17 @@ import { ImageContainer } from "./styles";
 import Image from "next/image";
 
 type DecoratedImageTypes = {
+  imageUrl: string;
+  imageDescription: string;
   className?: string;
 };
 
-const DecoratedImage = ({ className }: DecoratedImageTypes) => {
+const DecoratedImage = ({ imageUrl, imageDescription, className }: DecoratedImageTypes) => {
   return (
     <ImageContainer className={className && className}>
       <Image
-        src="/images/teste.png"
-        alt="teste"
+        src={imageUrl}
+        alt={imageDescription}
         fill={true}
         style={{ position: "absolute" }}
       />
--- components/ui/DecoratedImage/styles.tsx
+++ components/ui/DecoratedImage/styles.tsx
@@ -4,9 +4,6 @@ export const ImageContainer = styled.div`
   position: relative;
   width: 100%;
   height: 300px;
-  border-width: 5px 5px 0px 0px;
-  border-style: solid;
-  border-color: #efdf64;
 
   @media (min-width: 400px){
     height: 400px;
