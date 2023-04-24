7facdc61a0415c6dc292ec174628487fa5af64e7
Lucas Nobre <lucasnobre37@outlook.com>
2023-04-24 19:54:56 -0300

    edit ContactSection layout

--- components/ContactSection/styles.tsx
+++ components/ContactSection/styles.tsx
@@ -5,22 +5,44 @@ export const ContactSectionContainer = styled.section`
   flex-direction: column;
   gap: 15%;
 
-  @media (min-width: 1000px) {
-    flex-direction: row;
-    justify-content: space-between;
-    margin-bottom: 100px;
+  margin-top: 20px;
+
+  @media (min-width: 600px) {
+    margin-top: 30px;
   }
 
-  .contactSectionParagraph {
-    text-align: center;
+  @media (min-width: 800px) {
+    margin-top: 40px;
+  }
+
+  @media (min-width: 1000px) {
+    margin-bottom: 100px;
   }
 `;
 
-export const Teste = styled.div`
+export const MainContent = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 1000px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+    gap: 25%;
+  }
+`;
+
+export const ButtonsContainer = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
   align-items: center;
+  margin: 25px 0 100px 0;
+
+  @media (min-width: 1000px) {
+    align-items: center;
+    width: 60%;
+  }
 
   .contactButton {
     width: 100%;
@@ -29,13 +51,12 @@ export const Teste = styled.div`
       margin-top: 20px;
     }
   }
-`;
 
-export const ButtonsContainer = styled.div`
-  margin: 25px 0 100px 0;
-  width: 100%;
+  .contactSectionParagraph {
+    margin-bottom: 50px;
 
-  @media (min-width: 1000px) {
-    width: 60%;
+    @media (min-width: 1000px) {
+      margin-bottom: 0;
+    }
   }
 `;
