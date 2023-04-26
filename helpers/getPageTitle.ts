7a69b988260bec256df9c6d6d359d579631a6ec9
Lucas Nobre <lucasnobre37@outlook.com>
2023-04-26 03:14:58 -0300

    improve getPageTitle helper funcion to format more than one word

--- helpers/getPageTitle.ts
+++ helpers/getPageTitle.ts
@@ -1,11 +1,11 @@
 const getPageTitle = (routerQuery: string | string[] | undefined) => {
-  return typeof routerQuery
-    ? routerQuery
-        ?.toString()[0]
-        .toUpperCase()
-        .concat(routerQuery?.toString().slice(1))
-        .concat(" | RB Ferragens")
-    : undefined;
+  return routerQuery
+    ?.toString()
+    .split(" ")
+    .map((item) => item[0].toUpperCase().concat(item.slice(1)))
+    .toString()
+    .replaceAll(",", " ")
+    .concat(" | RB Ferragens");
 };
 
 export default getPageTitle;
