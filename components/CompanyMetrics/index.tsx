40da2e451b05929af1e7796cd36c9a41eba41685
Lucas Nobre <lucasnobre37@outlook.com>
2023-04-28 02:35:22 -0300

    add className support to CompanyMetrics component

--- components/CompanyMetrics/index.tsx
+++ components/CompanyMetrics/index.tsx
@@ -2,6 +2,10 @@ import { MetricsContainer } from "./styles";
 import NumData from "../ui/NumData";
 import { metrics } from "@/storage/data";
 
+type CompanyMetricsTypes = {
+  className?: string;
+};
+
 const metricsVariants = {
   hidden: {
     scale: 0,
@@ -20,13 +24,14 @@ const metricsVariants = {
     },
   },
 };
-const CompanyMetrics = () => {
+const CompanyMetrics = ({ className = "" }: CompanyMetricsTypes) => {
   return (
     <MetricsContainer
       variants={metricsVariants}
       initial="hidden"
       whileInView="show"
       viewport={{ once: true }}
+      className={className}
     >
       {metrics.map((metric) => {
         return (
