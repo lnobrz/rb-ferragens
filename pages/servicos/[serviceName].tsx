5d3dc7cdd9cacb39f709fe4c5026ad5e11e371ae
Lucas Nobre <lucasnobre37@outlook.com>
2023-04-28 02:33:13 -0300

    create services details page

--- pages/servicos/[serviceName].tsx
+++ pages/servicos/[serviceName].tsx
@@ -1,15 +1,45 @@
 import { useRouter } from "next/router";
+import Head from "next/head";
+import { metaDescriptions } from "@/storage/data";
+import getPageTitle from "@/helpers/getPageTitle";
+import MobileProductDetails from "@/components/MobileProductDetails";
+import { services } from "@/storage/data";
+import DesktopProductDetails from "@/components/DesktopProductDetails";
+import useDeviceInfo from "@/helpers/useDeviceInfo";
 
 const Service = () => {
   const router = useRouter();
-  const { serviceName } = router.query;
+  const device = useDeviceInfo();
+  const serviceName = router.query.serviceName?.toString().replaceAll("-", " ");
+  const pageTitle = getPageTitle(serviceName);
+  const serviceDetails = services.filter((service) => {
+    return service.name === serviceName;
+  })[0];
 
   return (
-    <h1>
-      {serviceName &&
-        typeof serviceName === "string" &&
-        serviceName.replaceAll("-", " ")}
-    </h1>
+    <>
+      {serviceDetails && (
+        <>
+          <Head>
+            <meta name="description" content={metaDescriptions.services} />
+            <title>{pageTitle}</title>
+          </Head>
+          {device === "mobile" ? (
+            <MobileProductDetails
+              productName={serviceDetails.name}
+              productImage={serviceDetails.imageUrl}
+              productDescription={serviceDetails.description}
+            />
+          ) : (
+            <DesktopProductDetails
+              productName={serviceDetails.name}
+              productImage={serviceDetails.imageUrl}
+              productDescription={serviceDetails.description}
+            />
+          )}
+        </>
+      )}
+    </>
   );
 };
 
