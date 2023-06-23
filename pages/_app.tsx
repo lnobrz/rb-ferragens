6b69eb6b54e020a6d379652f803381bfc1d577c2
Lucas Nobre <lucasnobre37@outlook.com>
2023-06-23 02:51:22 -0300

    add vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,6 +5,7 @@ import Header from "@/components/ui/Header";
 import Footer from "@/components/Footer";
 import Layout from "@/components/ui/Layout";
 import { useRouter } from "next/router";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = ({ Component, pageProps }: AppProps) => {
   const currentPage = useRouter().pathname;
@@ -19,6 +20,7 @@ const App = ({ Component, pageProps }: AppProps) => {
       )}
       <AnimatePresence>
         <Component {...pageProps} />
+        <Analytics />
       </AnimatePresence>
       <Layout>
         <Footer />
