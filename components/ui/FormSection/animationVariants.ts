a26fe8a701286a60d15fe08e93658dc9870ddde7
Lucas Nobre <lucasnobre37@outlook.com>
2023-08-09 02:51:32 -0300

    improve FormSection  responsiveness

---
+++ components/ui/FormSection/animationVariants.ts
@@ -0,0 +1,12 @@
+export const animationVariants = {
+  hidden: {
+    opacity: 0,
+    y: 20,
+    transition: { duration: 0.7 },
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { type: "spring", stiffness: 300, damping: 24 },
+  },
+};
